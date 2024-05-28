fc309164ea7b66747d32c610f49f0ebd4a9452b3
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-05-27 21:07:30 -0300

    git init

---
+++ mypersonalfinance-backend/src/main/java/com/wellingtonm/mypersonalfinance/MypersonalfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.wellingtonm.mypersonalfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypersonalfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypersonalfinanceApplication.class, args);
+	}
+
+}
---
+++ mypersonalfinance-backend/src/test/java/com/wellingtonm/mypersonalfinance/MypersonalfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellingtonm.mypersonalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypersonalfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
